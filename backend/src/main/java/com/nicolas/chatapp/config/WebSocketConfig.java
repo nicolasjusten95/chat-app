60faae583c6df3bf6127a4aa9bd17cada702f968
Nicolas Justen <nicolas.justen@sivis.com>
2024-04-10 15:56:50 +0200

    adjust websocket config

--- backend/src/main/java/com/nicolas/chatapp/config/WebSocketConfig.java
+++ backend/src/main/java/com/nicolas/chatapp/config/WebSocketConfig.java
@@ -11,14 +11,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 
 }
--- backend/src/main/java/com/nicolas/chatapp/controllers/ChatController.java
+++ backend/src/main/java/com/nicolas/chatapp/controllers/ChatController.java
@@ -1,6 +1,8 @@
 package com.nicolas.chatapp.controllers;
 
 import com.nicolas.chatapp.model.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,10 +12,11 @@ import org.springframework.stereotype.Controller;
 import java.util.Objects;
 
 @Controller
-
 public class ChatController {
 
-    @MessageMapping("/chat.sendMessage")
+    private static final Logger log = LoggerFactory.getLogger(ChatController.class);
+
+    @MessageMapping("/chat/sendMessage")
     @SendTo("/topic/public")
     public Message sendMessage(@Payload Message message) {
         return message;
@@ -24,6 +27,13 @@ public class ChatController {
     public Message addUser(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
         Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", message.getSender());
         return message;
+    }
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public String greeting(String message) {
+        log.info("Message received: {}", message);
+        return "Hello " + message + "!";
     }
 
 }
