ef64c4210577b43231c283b9d22b40e6024ac327
Nicolas Justen <nicolas.justen@sivis.com>
2024-04-18 13:42:08 +0200

    adjusted home controller

--- backend/src/main/java/com/nicolas/chatapp/controllers/HomeController.java
+++ backend/src/main/java/com/nicolas/chatapp/controllers/HomeController.java
@@ -1,19 +1,19 @@
 package com.nicolas.chatapp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/home")
 public class HomeController {
 
-    @GetMapping
-    public String hello() {
-        log.info("hello world!");
-        return "hello world!";
+    @GetMapping("/")
+    public ResponseEntity<String> home() {
+        return new ResponseEntity<>("Welcome to my chat app", HttpStatus.OK);
     }
 
 }
