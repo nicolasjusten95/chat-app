2825a537fd119032691d335defd3142856e7bfc9
Nicolas Justen <nicolas.justen@sivis.com>
2024-05-07 15:55:28 +0200

    added sample data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -30,7 +30,9 @@ VALUES ('0bd20a41-4d23-4c4e-a8aa-8e46743f9ee4', 'Darth Vader and Luke', false, '
        ('c40e7df3-7e67-4955-96b5-25e8769ec9bc', 'Luke and Leia', false, 'be900497-cc68-4504-9b99-4e5deaf1e6c0'),
        ('ac63914e-151e-444f-b44c-f67a3374f1f1', 'The Goodies', true, 'be900497-cc68-4504-9b99-4e5deaf1e6c0'),
        ('f476eee8-9a39-4fd2-906f-9e7a746ef167', 'The Dark Side', true, 'c419a854-010a-4a50-be82-f4587014d6e4'),
-       ('8a3ad4c8-3c57-43c3-aed7-f3af68da5135', 'Leia and Kenobi', false, 'd7083ad6-9e09-453e-b7c8-65016f20ea37');
+       ('8a3ad4c8-3c57-43c3-aed7-f3af68da5135', 'Leia and Kenobi', false, 'd7083ad6-9e09-453e-b7c8-65016f20ea37'),
+       ('7d81a13c-e835-455a-bc99-3fb6001fb0d5', 'Help from Yoda', false, '6f8e857c-c22f-44a1-bd97-8c2f9ce688d7'),
+       ('74397056-5ede-4533-8dfe-5d8367d588d2', 'Chewie and Han', false, 'cd467a4d-8430-4545-ad3a-634ce007af46');
 
 
 INSERT INTO CHAT_ADMINS(admins_id, chat_id)
@@ -50,7 +52,11 @@ VALUES ('0bd20a41-4d23-4c4e-a8aa-8e46743f9ee4', 'f290f384-60ba-4cdd-af96-26c88ed
        ('f476eee8-9a39-4fd2-906f-9e7a746ef167', 'f290f384-60ba-4cdd-af96-26c88ede0264'),
        ('f476eee8-9a39-4fd2-906f-9e7a746ef167', 'c419a854-010a-4a50-be82-f4587014d6e4'),
        ('8a3ad4c8-3c57-43c3-aed7-f3af68da5135', '0fb97ac1-1304-4e83-b640-f659b8679907'),
-       ('8a3ad4c8-3c57-43c3-aed7-f3af68da5135', 'd7083ad6-9e09-453e-b7c8-65016f20ea37');
+       ('8a3ad4c8-3c57-43c3-aed7-f3af68da5135', 'd7083ad6-9e09-453e-b7c8-65016f20ea37'),
+       ('7d81a13c-e835-455a-bc99-3fb6001fb0d5', '6f8e857c-c22f-44a1-bd97-8c2f9ce688d7'),
+       ('7d81a13c-e835-455a-bc99-3fb6001fb0d5', 'be900497-cc68-4504-9b99-4e5deaf1e6c0'),
+       ('74397056-5ede-4533-8dfe-5d8367d588d2', 'cd467a4d-8430-4545-ad3a-634ce007af46'),
+       ('74397056-5ede-4533-8dfe-5d8367d588d2', '4e039f0a-5eaf-4354-ad5b-14e2889643d4');
 
 
 INSERT INTO MESSAGE(id, content, time_stamp, user_id, chat_id)
@@ -58,7 +64,10 @@ VALUES ('a284a44a-7b28-45da-8463-3a35417715f0', 'I am your father', '2024-04-22 
        ('37afbdc4-89b4-4961-b825-bb4d666e5442', 'Noooo', '2024-04-22 20:02:08.535241 +00:00', 'be900497-cc68-4504-9b99-4e5deaf1e6c0', '0bd20a41-4d23-4c4e-a8aa-8e46743f9ee4'),
        ('620d606a-9033-4210-b9c0-982e0f3800ef', 'Oh btw I am your sister', '2024-04-22 20:03:08.535241 +00:00', '0fb97ac1-1304-4e83-b640-f659b8679907', 'c40e7df3-7e67-4955-96b5-25e8769ec9bc'),
        ('15733d9e-939d-497b-b042-fd2fe54d7430', 'Good to know', '2024-04-22 20:04:08.535241 +00:00', 'be900497-cc68-4504-9b99-4e5deaf1e6c0', 'c40e7df3-7e67-4955-96b5-25e8769ec9bc'),
-       ('6bd25bf8-dba1-46b1-8821-ba838d4a84ae', 'We won!', '2024-04-22 20:05:08.535241 +00:00', 'be900497-cc68-4504-9b99-4e5deaf1e6c0', 'ac63914e-151e-444f-b44c-f67a3374f1f1');
+       ('6bd25bf8-dba1-46b1-8821-ba838d4a84ae', 'We won!', '2024-04-22 20:05:08.535241 +00:00', 'be900497-cc68-4504-9b99-4e5deaf1e6c0', 'ac63914e-151e-444f-b44c-f67a3374f1f1'),
+       ('08db069b-e3d5-4cff-b17e-b3af15bb667f', 'The force will be with you, always', '2024-04-23 11:33:07.535241 +00:00', '6f8e857c-c22f-44a1-bd97-8c2f9ce688d7', '7d81a13c-e835-455a-bc99-3fb6001fb0d5'),
+       ('1b5bd428-9cc7-480b-8f8c-6cee22e6b76e', 'I got a really bad feeling about this', '2024-04-23 11:35:08.535241 +00:00', '4e039f0a-5eaf-4354-ad5b-14e2889643d4', '74397056-5ede-4533-8dfe-5d8367d588d2'),
+       ('81c67b8c-51f4-46e3-b7eb-8c73db374a2d', 'RRWWWGG', '2024-04-23 11:36:09.535241 +00:00', 'cd467a4d-8430-4545-ad3a-634ce007af46', '74397056-5ede-4533-8dfe-5d8367d588d2');
 
 
 INSERT INTO CHAT_MESSAGES(chat_id, messages_id)
@@ -66,4 +75,7 @@ VALUES ('0bd20a41-4d23-4c4e-a8aa-8e46743f9ee4', 'a284a44a-7b28-45da-8463-3a35417
        ('0bd20a41-4d23-4c4e-a8aa-8e46743f9ee4', '37afbdc4-89b4-4961-b825-bb4d666e5442'),
        ('c40e7df3-7e67-4955-96b5-25e8769ec9bc', '620d606a-9033-4210-b9c0-982e0f3800ef'),
        ('c40e7df3-7e67-4955-96b5-25e8769ec9bc', '15733d9e-939d-497b-b042-fd2fe54d7430'),
-       ('ac63914e-151e-444f-b44c-f67a3374f1f1', '6bd25bf8-dba1-46b1-8821-ba838d4a84ae');
+       ('ac63914e-151e-444f-b44c-f67a3374f1f1', '6bd25bf8-dba1-46b1-8821-ba838d4a84ae'),
+       ('7d81a13c-e835-455a-bc99-3fb6001fb0d5', '08db069b-e3d5-4cff-b17e-b3af15bb667f'),
+       ('74397056-5ede-4533-8dfe-5d8367d588d2', '1b5bd428-9cc7-480b-8f8c-6cee22e6b76e'),
+       ('74397056-5ede-4533-8dfe-5d8367d588d2', '81c67b8c-51f4-46e3-b7eb-8c73db374a2d');
